a84cda23e84f9082799e64e3ed509e0b275296bf
Freedom J Scheyd <joefreedomsch@gmail.com>
2020-07-17 23:59:58 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,10 +15,10 @@
 def speak_to_grandma(phrase)
   if phrase == phrase.downcase
     return "HUH?! SPEAK UP, SONNY!"
-  elsif phrase.upcase
-    return "NO, NOT SINCE 1938!"
   elsif phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
+  else
+    return "NO, NOT SINCE 1938!"
   end
 end
 
