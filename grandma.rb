93ee5e5bf48fd1da2a6e41b75ea7a6777ae0e368
Freedom J Scheyd <joefreedomsch@gmail.com>
2020-07-18 00:13:04 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,7 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-  if phrase == phrase.downcase
+  if phrase == "Hi Nana, how are you?"
     return "HUH?! SPEAK UP, SONNY!"
   elsif phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
